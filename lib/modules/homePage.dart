42f6af55ea29bb81c00b6bfb917d4f591af6be85
Hanieh Ghassemi <haniehghsmie@gmail.com>
2023-07-08 09:35:51 +0330

    feat: :sparkles: (HN-11): change ui

--- lib/modules/homePage.dart
+++ lib/modules/homePage.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_4/modules/product_details.dart';
 import 'package:http/http.dart' as http;
 
+import '../models/product.dart';
 import 'product_list.dart';
 
 class HomePage extends StatefulWidget {
@@ -86,6 +88,7 @@ class _HomePageState extends State<HomePage> {
                         ),
                         child: Center(
                           child: Text(
+                            textAlign: TextAlign.center,
                             categories[index],
                             style: TextStyle(
                               fontSize: 16,
@@ -100,12 +103,12 @@ class _HomePageState extends State<HomePage> {
                 },
               ),
             ),
-            SizedBox(height: 16),
+            SizedBox(height: 12),
             Text(
               'New Products',
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 16),
+            SizedBox(height: 20),
             Expanded(
               child: GridView.builder(
                 itemCount: newProducts.length,
@@ -117,266 +120,75 @@ class _HomePageState extends State<HomePage> {
                 ),
                 itemBuilder: (context, index) {
                   return Container(
+                    padding: EdgeInsets.all(8),
                     decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Expanded(
-                          child: Container(
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                image: NetworkImage(newProducts[index].image),
-                                fit: BoxFit.cover,
-                              ),
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                newProducts[index].title,
-                                maxLines: 1,
-                                overflow: TextOverflow.ellipsis,
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              SizedBox(height: 4),
-                              Text(
-                                '${newProducts[index].price}\$',
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  color: Colors.grey[600],
-                                ),
-                              ),
-                            ],
-                          ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.3),
+                          spreadRadius: 1,
+                          blurRadius: 7,
+                          offset: Offset(0, 3),
                         ),
                       ],
                     ),
+                    child: GestureDetector(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Expanded(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    image:
+                                        NetworkImage(newProducts[index].image),
+                                    fit: BoxFit.cover,
+                                  ),
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    newProducts[index].title,
+                                    maxLines: 1,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(height: 4),
+                                  Text(
+                                    '${newProducts[index].price}\$',
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.grey[600],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  ProductPage(productId: newProducts[index].id),
+                            ),
+                          );
+                        }),
                   );
                 },
               ),
             ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-// class CategoryPage extends StatefulWidget {
-//   final String categoryName;
-
-//   CategoryPage({required this.categoryName});
-
-//   @override
-//   _CategoryPageState createState() => _CategoryPageState();
-// }
-
-// class _CategoryPageState extends State<CategoryPage> {
-//   List<Product> categoryProducts = [];
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     fetchCategoryProducts();
-//   }
-
-//   Future<void> fetchCategoryProducts() async {
-//     final response = await http.get(Uri.parse(
-//         'https://fakestoreapi.com/products/category/${widget.categoryName}'));
-//     if (response.statusCode == 200) {
-//       setState(() {
-//         Iterable list = json.decode(response.body);
-//         categoryProducts =
-//             list.map((model) => Product.fromJson(model)).toList();
-//       });
-//     } else {
-//       throw Exception('Failed to load category products');
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: GridView.builder(
-//           itemCount: categoryProducts.length,
-//           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-//             crossAxisCount: 2,
-//             childAspectRatio: 0.75,
-//             crossAxisSpacing: 16,
-//             mainAxisSpacing: 16,
-//           ),
-//           itemBuilder: (context, index) {
-//             return Container(
-//               decoration: BoxDecoration(
-//                 color: Colors.white,
-//                 borderRadius: BorderRadius.circular(8),
-//               ),
-//               child: Column(
-//                 crossAxisAlignment: CrossAxisAlignment.start,
-//                 children: [
-//                   Expanded(
-//                     child: Container(
-//                       decoration: BoxDecoration(
-//                         image: DecorationImage(
-//                           image: NetworkImage(categoryProducts[index].image),
-//                           fit: BoxFit.cover,
-//                         ),
-//                         borderRadius: BorderRadius.circular(8),
-//                       ),
-//                     ),
-//                   ),
-//                   Padding(
-//                     padding: const EdgeInsets.symmetric(horizontal: 8.0),
-//                     child: Column(
-//                       crossAxisAlignment: CrossAxisAlignment.start,
-//                       children: [
-//                         Text(
-//                           categoryProducts[index].title,
-//                           maxLines: 1,
-//                           overflow: TextOverflow.ellipsis,
-//                           style: TextStyle(
-//                             fontSize: 16,
-//                             fontWeight: FontWeight.bold,
-//                           ),
-//                         ),
-//                         SizedBox(height: 4),
-//                         Text(
-//                           '${categoryProducts[index].price}\$',
-//                           style: TextStyle(
-//                             fontSize: 14,
-//                             color: Colors.grey[600],
-//                           ),
-//                         ),
-//                       ],
-//                     ),
-//                   ),
-//                 ],
-//               ),
-//             );
-//           },
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-class Product {
-  final int id;
-  final String title;
-  final double price;
-  final String image;
-
-  Product(
-      {required this.id,
-      required this.title,
-      required this.price,
-      required this.image});
-
-  factory Product.fromJson(Map<String, dynamic> json) {
-    return Product(
-      id: json['id'],
-      title: json['title'],
-      price: json['price'].toDouble(),
-      image: json['image'],
-    );
-  }
-}
-
-class ProductPage extends StatefulWidget {
-  final int productId;
-
-  ProductPage({required this.productId});
-
-  @override
-  _ProductPageState createState() => _ProductPageState();
-}
-
-class _ProductPageState extends State<ProductPage> {
-  Product? product;
-
-  @override
-  void initState() {
-    super.initState();
-    fetchProduct();
-  }
-
-  Future<void> fetchProduct() async {
-    final response = await http.get(
-        Uri.parse('https://fakestoreapi.com/products/${widget.productId}'));
-    if (response.statusCode == 200) {
-      setState(() {
-        product = Product.fromJson(json.decode(response.body));
-      });
-    } else {
-      throw Exception('Failed to load product');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (product == null) {
-      return Scaffold(
-        body: Center(
-          child: CircularProgressIndicator(),
-        ),
-      );
-    }
-
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: NetworkImage(product!.image),
-                    fit: BoxFit.cover,
-                  ),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-              ),
-            ),
-            SizedBox(height: 16),
-            Text(
-              product!.title,
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 8),
-            Text(
-              '${product!.price}\$',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.grey[600],
-              ),
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Description:',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 8),
           ],
         ),
       ),
--- lib/modules/product_details.dart
+++ lib/modules/product_details.dart
@@ -3,12 +3,15 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ProductPage extends StatefulWidget {
+  int? productId;
+  ProductPage({Key? key, this.productId}) : super(key: key);
+
   @override
   _ProductPageState createState() => _ProductPageState();
 }
 
 class _ProductPageState extends State<ProductPage> {
-  late Map<String, dynamic> productData;
+  Map<String, dynamic>? productData;
 
   @override
   void initState() {
@@ -17,8 +20,8 @@ class _ProductPageState extends State<ProductPage> {
   }
 
   Future getData() async {
-    var response =
-        await http.get(Uri.parse('https://fakestoreapi.com/products/1'));
+    var response = await http.get(
+        Uri.parse('https://fakestoreapi.com/products/${widget.productId}'));
     if (response.statusCode == 200) {
       setState(() {
         productData = json.decode(response.body);
@@ -31,100 +34,99 @@ class _ProductPageState extends State<ProductPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: productData == null
-          ? Center(child: CircularProgressIndicator())
-          : SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Container(
-                    margin: EdgeInsets.all(16),
-                    height: 300,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(16),
-                      border: Border.all(color: Colors.grey[400]!),
-                    ),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(16),
-                      child: Image.network(
-                        productData['image'],
-                        fit: BoxFit.cover,
+        body: productData == null
+            ? Center(child: CircularProgressIndicator())
+            : SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Container(
+                      height: 500,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(16),
+                        child: Image.network(
+                          productData?['image'],
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: Text(
-                      productData['title'],
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
+                    SizedBox(height: 18),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      child: Text(
+                        productData?['title'],
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: Row(
-                      children: [
-                        Text(
-                          '\$${productData['price']}',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.red[800],
+                    Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Category: ${productData?['category']}',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: const Color.fromARGB(255, 98, 98, 98),
+                            ),
                           ),
-                        ),
-                        SizedBox(width: 8),
-                        Text(
-                          '\$${productData['price']}',
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.grey[600],
+                          SizedBox(height: 8),
+                          Text(
+                            productData?['description'],
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey[600],
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Category: ${productData['category']}',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.grey[600],
-                          ),
-                        ),
-                        SizedBox(height: 8),
-                        Text(
-                          productData['description'],
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.grey[600],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 16),
-                  Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        addToCart();
-                      },
-                      style: ElevatedButton.styleFrom(
-                        primary: Color(0xFFF44336),
+                        ],
                       ),
-                      child: Text('Add to Cart'),
                     ),
-                  ),
-                ],
+                    SizedBox(height: 16),
+                  ],
+                ),
               ),
-            ),
-    );
+        bottomNavigationBar: NavigationBarTheme(
+          data: NavigationBarThemeData(
+            indicatorColor: Colors.transparent,
+          ),
+          child: BottomNavigationBar(
+            backgroundColor: Colors.grey[100],
+            items: [
+              BottomNavigationBarItem(
+                icon: Text(
+                  '\$${productData?['price']}',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFFF44336),
+                  ),
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: IconButton(
+                  icon: Icon(Icons.shopping_cart_outlined),
+                  selectedIcon: Icon(
+                    Icons.supervisor_account,
+                    color: Color(0xFFF44336),
+                  ),
+                  onPressed: () {
+                    addToCart();
+                  },
+                ),
+                label: '',
+              )
+            ],
+            onTap: (index) {
+              if (index == 1) {
+                addToCart();
+              }
+            },
+          ),
+        ));
   }
 
   Future addToCart() async {
