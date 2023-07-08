8b217b621e4803d4d37b768e46c895b1903fa6a2
Hanieh Ghassemi <haniehghsmie@gmail.com>
2023-07-08 09:57:21 +0330

    feat: :sparkles: (HN-7): add icons to this page

--- lib/modules/about_us.dart
+++ lib/modules/about_us.dart
@@ -18,7 +18,7 @@ class _AboutUsState extends State<AboutUs> {
           children: <Widget>[
             SizedBox(height: 16),
             Image(
-              image: AssetImage('assets/img/buyme.png'),
+              image: AssetImage('img/buyme.png'),
               height: 200,
             ),
             SizedBox(height: 32),
@@ -36,30 +36,30 @@ class _AboutUsState extends State<AboutUs> {
             SizedBox(height: 32),
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 16.0),
-              child: Align(
-                alignment: Alignment.topLeft,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0),
-                  child: Text(
-                    'Phone: +44 123 456 7890',
+              child: Row(
+                children: [
+                  Icon(Icons.phone, size: 24, color: Color(0xFFF44336)),
+                  SizedBox(width: 8),
+                  Text(
+                    '+44 123 456 7890',
                     style: TextStyle(fontSize: 18),
                     textAlign: TextAlign.left,
                   ),
-                ),
+                ],
               ),
             ),
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 16.0),
-              child: Align(
-                alignment: Alignment.topLeft,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0),
-                  child: Text(
-                    'Address: 123 Main St, London, UK',
+              child: Row(
+                children: [
+                  Icon(Icons.location_on, size: 24, color: Color(0xFFF44336)),
+                  SizedBox(width: 8),
+                  Text(
+                    '123 Main St, London, UK',
                     style: TextStyle(fontSize: 18),
                     textAlign: TextAlign.left,
                   ),
-                ),
+                ],
               ),
             ),
           ],
